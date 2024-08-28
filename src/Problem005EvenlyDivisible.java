68075459c4e0aa374029e48f3177a3ed0108b7e3
Lourenzo Ferreira <lourenzo.ferreira@comprealugueagora.com.br>
2024-08-28 16:05:16 -0300

    feat: solution for problem 005

--- src/Problem005EvenlyDivisible.java
+++ src/Problem005EvenlyDivisible.java
@@ -1,10 +1,13 @@
-import java.util.stream.IntStream;
+import static java.util.stream.IntStream.iterate;
+import static java.util.stream.IntStream.rangeClosed;
 
 public class Problem005EvenlyDivisible {
 
   public static void main(String[] args) {
-    Integer n = IntStream.iterate(2520, i -> i + 1)
-      .filter(x -> IntStream.rangeClosed(1, 20).allMatch(y -> x % y == 0)).findFirst().orElseThrow();
+    Integer n = iterate(2520, i -> i + 1)
+      .filter(x -> rangeClosed(1, 20).allMatch(y -> x % y == 0))
+      .findFirst().orElseThrow();
+
     System.out.println(n);
   }
 }
