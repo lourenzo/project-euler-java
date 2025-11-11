a04e8e25128d820f0096eeb7c69181b6a67f99c3
Lourenzo Ferreira <lourenzo@gmail.com>
2025-11-11 14:15:10 -0300

    refactor: rewrite solution for problem 005 to use compact java source files

--- src/Problem005EvenlyDivisible.java
+++ src/Problem005EvenlyDivisible.java
@@ -1,13 +1,10 @@
 import static java.util.stream.IntStream.iterate;
 import static java.util.stream.IntStream.rangeClosed;
 
-public class Problem005EvenlyDivisible {
+void main() {
+  Integer n = iterate(2520, i -> i + 20)
+    .filter(x -> rangeClosed(1, 20).allMatch(y -> x % y == 0))
+    .findFirst().orElseThrow();
 
-  public static void main(String[] args) {
-    Integer n = iterate(2520, i -> i + 20)
-      .filter(x -> rangeClosed(1, 20).allMatch(y -> x % y == 0))
-      .findFirst().orElseThrow();
-
-    System.out.println(n);
-  }
+  IO.println(n);
 }
