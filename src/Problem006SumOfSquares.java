d2faaa848ad42590388055342b8da52b0c57b82f
Lourenzo Ferreira <lourenzo@gmail.com>
2025-11-11 14:15:36 -0300

    refactor: rewrite solution for problem 006 to use compact java source files

--- src/Problem006SumOfSquares.java
+++ src/Problem006SumOfSquares.java
@@ -1,11 +1,8 @@
 import static java.lang.Math.pow;
 import static java.util.stream.IntStream.rangeClosed;
 
-public class Problem006SumOfSquares {
-
-  public static void main(String[] args) {
-    System.out.println(
-      (int) pow(rangeClosed(1, 100).sum(), 2) - rangeClosed(1, 100).map(x -> (int) pow(x, 2)).sum()
-    );
-  }
+void main() {
+  IO.println(
+    (int) pow(rangeClosed(1, 100).sum(), 2) - rangeClosed(1, 100).map(x -> (int) pow(x, 2)).sum()
+  );
 }
