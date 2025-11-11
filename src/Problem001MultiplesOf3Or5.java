70543e7d74160b0bd43159d53ef85f9ce068ef09
Lourenzo Ferreira <lourenzo@gmail.com>
2025-11-11 01:40:32 -0300

    refactor: rewrite problem 001 solution to compact source file

--- src/Problem001MultiplesOf3Or5.java
+++ src/Problem001MultiplesOf3Or5.java
@@ -1,13 +1,8 @@
-import java.util.OptionalInt;
-import java.util.stream.IntStream;
+void main() {
+  OptionalInt result = IntStream
+    .range(1, 1000)
+    .filter(x -> x % 3 == 0 || x % 5 == 0)
+    .reduce(Integer::sum);
 
-public class Problem001MultiplesOf3Or5 {
-  public static void main(String[] args) {
-    OptionalInt result = IntStream
-      .range(1, 1000)
-      .filter(x -> x % 3 == 0 || x % 5 == 0)
-      .reduce(Integer::sum);
-
-    System.out.println("Result: " + result);
-  }
+  IO.println("Result: " + result.orElseThrow());
 }
