2c3935e2a4146605e340c49e75d3388fc93192d1
Lourenzo Ferreira <lourenzo@gmail.com>
2025-11-11 09:56:26 -0300

    refactor: rewrite problem 003 solution to compact source file

--- src/PrimeToolKit.java
+++ src/PrimeToolKit.java
@@ -4,12 +4,12 @@ import static java.util.stream.LongStream.range;
 public class PrimeToolKit {
 
   // From exercise 003
-  public static boolean isPrime(long number) {
+  static boolean isPrime(long number) {
     return number > 1 && range(2, Math.round(Math.sqrt(number))).noneMatch(i -> number % i == 0);
   }
 
   // From exercise 007
-  public static int nthPrime(int n) {
+  static int nthPrime(int n) {
     return iterate(1, x -> x + 1)
         .filter(PrimeToolKit::isPrime)
         .limit(n).toArray()[n - 1];
--- src/Problem003LargestPrimeFactor.java
+++ src/Problem003LargestPrimeFactor.java
@@ -1,26 +1,14 @@
-import java.util.OptionalInt;
-
 import static java.util.stream.IntStream.range;
 import static java.util.stream.LongStream.range;
 
-public class Problem003LargestPrimeFactor {
+void main() {
+  long n = 600851475143L;
 
-  private static boolean isPrime(long number) {
-    return number > 1 && range(2, Math.round(Math.sqrt(number))).noneMatch(i -> number % i == 0);
-  }
+  OptionalInt result = range(1, (int) Math.sqrt(n))
+    .filter(x -> n % x == 0)
+    .filter(number -> number > 1 && range(2, Math.round(Math.sqrt(number))).noneMatch(i -> number % i == 0))
+    .takeWhile(x -> x < Math.sqrt(n))
+    .reduce((x, y) -> y);
 
-  public static void main(String[] args) {
-    long n = 600851475143L;
-
-    OptionalInt result = range(1, (int) Math.sqrt(n))
-      .filter(x -> n % x == 0)
-      .filter(Problem003LargestPrimeFactor::isPrime)
-      .takeWhile(x -> x < Math.sqrt(n))
-      .reduce((x, y) -> y);
-
-    System.out.println(result);
-
-    //List<Long> primesUntil = primeNumbersUntil(n);
-    //System.out.println(primesUntil.get(primesUntil.size() - 1));
-  }
+  IO.println(result.orElseThrow());
 }
