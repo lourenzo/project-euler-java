1572aba7321f2759ccf60474e42007478076dff8
Lourenzo Ferreira <lourenzo@gmail.com>
2025-11-11 14:10:50 -0300

    refactor: rewrite solution for problem 004 to use compact java source files

--- src/Problem004PalindromicNumber.java
+++ src/Problem004PalindromicNumber.java
@@ -1,27 +1,15 @@
-import java.util.Optional;
-
 import static java.util.stream.IntStream.iterate;
 
-public class Problem004PalindromicNumber {
+void main() {
+  Optional<Integer> num = iterate(999, x -> x > 900, x -> x - 1)
+    .mapToObj(x -> iterate(999, y -> y > 900, y -> y - 1)
+      .map(y -> x * y)
+      .distinct()
+      .filter((int number) -> String.valueOf(number)
+        .equals(new StringBuilder(String.valueOf(number)).reverse().toString()))
+      .boxed())
+    .flatMap(stream -> stream)
+    .max(Integer::compareTo);
 
-  public static boolean isPalindrome(int number) {
-    return String
-      .valueOf(number)
-      .equals(new StringBuilder(String.valueOf(number)).reverse().toString());
-  }
-
-  public static void main(String[] args) {
-    Optional<Integer> num = iterate(999, x -> x > 900, x -> x - 1)
-      .mapToObj(x -> iterate(999, y -> y > 900, y -> y - 1)
-        .map(y -> x * y)
-        .distinct()
-        .filter(Problem004PalindromicNumber::isPalindrome)
-        .boxed())
-      .flatMap(stream -> stream)
-      .max(Integer::compareTo);
-
-    System.out.println(num);
-  }
-
-  // Improvements: make the range descending, from 999 to 900 is enough
+  IO.println(num.orElseThrow());
 }
